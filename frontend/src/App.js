24eae3c8416b36705f81b12fc6b35fa805ebb0c9
U01ABD0D <U01ABD0D@lacaixa.es>
2025-04-22 16:25:35 +0200

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
+const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 const App = () => {
+  const [word,setWord] = useState('');
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.unsplash.com/photos/random?query?${word}&client_id=${UNSPLASH_ACCESS_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data from Unsplash:', error);
+      });
+
+  }
+
+
   return (
     <div className="App">
       <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //In strict mode React executes two times the components.
+  //Commented Strict Mode to avoid reloads in development mode
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
